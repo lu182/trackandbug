72e9ad4d0090f3ddfb0140f65df75c5271c7f809
Luciana <laf.0182@gmail.com>
2021-04-27 16:29:17 -0300

    commit inicial

---
+++ src/main/java/edu/curso/java/trackandbug/TrackandbugApplication.java
@@ -0,0 +1,14 @@
+package edu.curso.java.trackandbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackandbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackandbugApplication.class, args);
+		System.out.println("Probando commit.....");
+	}
+
+}
---
+++ src/test/java/edu/curso/java/trackandbug/TrackandbugApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.curso.java.trackandbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackandbugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
